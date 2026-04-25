c7cf57cd72a8ac555fc5a0416f6dedd5508ab952
YunseoLee <yunseolee.dev@gmail.com>
2026-04-25 16:45:36 +0900

    [UPDATE] add offsets_opt, fuzz_times, arg1, arg2 to cond_queue.csv

--- fuzzer/src/depot/dump.rs
+++ fuzzer/src/depot/dump.rs
@@ -10,7 +10,7 @@ impl Drop for Depot {
         let mut log_q = fs::File::create(dir.join(defs::COND_QUEUE_FILE)).unwrap();
         writeln!(
             log_q,
-            "cmpid, context, order, belong, p, op, condition, is_desirable, offsets, state"
+            "cmpid, context, order, belong, p, op, condition, arg1, arg2, is_desirable, fuzz_times, offsets, offsets_opt, state"
         )
         .unwrap();
         let q = self.queue.lock().unwrap();
@@ -22,9 +22,14 @@ impl Drop for Depot {
                     offsets.push(format!("{}-{}", off.begin, off.end));
                 }
 
+                let mut offsets_opt = vec![];
+                for off in &cond.offsets_opt {
+                    offsets_opt.push(format!("{}-{}", off.begin, off.end));
+                }
+
                 writeln!(
                     log_q,
-                    "{}, {}, {}, {}, {}, {}, {}, {:x}, {:x}, {}, {}, {:?}",
+                    "{}, {}, {}, {}, {}, {}, {}, {:x}, {:x}, {}, {}, {}, {}, {:?}",
                     cond.base.cmpid,
                     cond.base.context,
                     cond.base.order,
@@ -35,7 +40,9 @@ impl Drop for Depot {
                     cond.base.arg1,
                     cond.base.arg2,
                     cond.is_desirable,
+                    cond.fuzz_times,
                     offsets.join("&"),
+                    offsets_opt.join("&"),
                     cond.state
                 )
                 .unwrap();
